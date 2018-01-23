be767c1a16982612e7bc38e6681eb6620629a846
hirichardhai <hirichardhai@gmail.com>
2018-01-22 20:23:14 -0800

    pass all tests with the pseudoclassical pattern

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new MakeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,6 +22,7 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
+      // console.log('callcount passed');
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,42 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  // var blinkyDancer = MakeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  // debugger;
+  // var oldStep = blinkyDancer.step;
+  console.log('MakeBlinkyDancer this:', this);
+  // this.oldStep = MakeDancer.prototype.step;
 
-  var oldStep = blinkyDancer.step;
+  // blinkyDancer.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   blinkyDancer.$node.toggle();
+  // };
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+  // return blinkyDancer;
+};
+// ForagerBee.prototype = Object.create(Bee.prototype);
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 
-  return blinkyDancer;
+MakeBlinkyDancer.prototype.oldStep = MakeDancer.prototype.step;
+
+MakeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  // debugger;
+  // MakeDancer.prototype.step.call(this);
+  console.log('MakeBlinkyDancer.prototype.step this:', this);
+  this.oldStep();
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+  // setTimeout(this.oldStep.bind(this), this.timeBetweenSteps);
+
+  // this.step()
 };
